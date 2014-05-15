eafb19ba1011d4618dbafaf3a668fdedb51deaa3
Daniel Bonebrake <danielbonebrake@gmail.com>
2014-05-15 16:59:58 -0500

    update

--- metro_restoration/urls.py
+++ metro_restoration/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import *
+from django.conf.urls import url, include, patterns
 from django.conf.urls.i18n import i18n_patterns
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.conf import settings
 from cms.sitemaps import CMSSitemap
@@ -18,4 +18,5 @@ if settings.DEBUG:
     urlpatterns = patterns('',
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-) + staticfiles_urlpatterns() + urlpatterns
+) + urlpatterns
+#) + staticfiles_urlpatterns() + urlpatterns
