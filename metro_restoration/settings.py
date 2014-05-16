1a1b06668246a992a270fa1531d471f5bffabf22
Daniel Bonebrake <danielbonebrake@gmail.com>
2014-05-16 17:01:57 -0500

    please work!

--- metro_restoration/settings.py
+++ metro_restoration/settings.py
@@ -19,7 +19,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, 'staticfiles'),
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 #=========== settings.env ================
